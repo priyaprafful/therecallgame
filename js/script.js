3f9bbe53a87d038b5eafa5b99a5ee36150c36816
priyaprafful <priyankamaheshwari.2021@gmail.com>
2018-10-31 20:46:00 +0200

    game finished phase 1

--- js/script.js
+++ js/script.js
@@ -1,16 +1,135 @@
- var computerOrder =[];
- var playerOrder =[];
-var flash;
- var turn;
- var good;
- var compTurn;
- var intervalId;
- var noise= true;
- var start;
- var win;
+class Computer {
+  constructor() {
+    this.computerOrder = [];
+    this.computerTurn = true;
+    this.turn = 1;
+    this.flash =0;
+    this.computerFlashing = false;
+  }
+
+  createComputerOrder() {
+    var options = ["1", "2", "3", "#","9","8","5","4","6","$","7","@"]
+    for (var i = 0; i < 20; i++) {
+      var index = Math.floor(Math.random() * options.length);
+      this.computerOrder.push(options[index]);
+    }
+    console.log(this.computerOrder);
+  }
+
+  flashCards() {
+      if (this.computerFlashing) {
+        return;
+      }
+
+      this.computerFlashing = true;
+    //console.log("flashcard called");
+
+   
+      //alert("flash is "+ this.flash);
+      var i = 0;
+      var number = setInterval(() => {
+
+        if (i > this.flash) {
+          clearInterval(number);
+          this.computerFlashing = false;
+          this.flash++;
+          return;
+        }
+
+        console.log(i, this.computerOrder[i])
+
+        
+        if (this.computerOrder[i] === "1") {
+          console.log("first if statement entered");
+          numberOne.style.backgroundColor = "lightblue";
+        
+        }
+      
+      
+        if (this.computerOrder[i] === "2") {
+          numberTwo.style.backgroundColor = "lightblue";
+       }
+      
+      
+        if (this.computerOrder[i] === "3") {
+          numberThree.style.backgroundColor = "lightblue";
+      }
+      
+      
+      
+        if (this.computerOrder[i] === "4") {
+          numberFour.style.backgroundColor = "lightblue";
+        }
+      
+      
+        if (this.computerOrder[i] === "5") {
+          numberFive.style.backgroundColor = "lightblue";
+      }
+      
+      
+        if (this.computerOrder[i] === "6") {
+          numberSix.style.backgroundColor = "lightblue";
+    }
+      
+        
+        if (this.computerOrder[i] === "7") {
+           numberSeven.style.backgroundColor = "lightblue";
+      }
+      
+        
+        if (this.computerOrder[i] === "8") {
+        numberEight.style.backgroundColor = "lightblue";
+   }
+      
+        
+        if (this.computerOrder[i] === "9") {
+          numberNine.style.backgroundColor = "lightblue";
+        }
+      
+        
+         if (this.computerOrder[i] === "#" ) {
+     
+           numberTen.style.backgroundColor = "lightblue";
+         }
+      
+        
+         if (this.computerOrder[i] === "@") {
+        
+           numberEleven.style.backgroundColor = "lightblue";
+         }
+      
+        
+         if (this.computerOrder[i] === "$") {
+       
+           numberTwelve.style.backgroundColor = "lightblue";
+         }
+         i++;
+         
+      setTimeout(function () {
+        clearColor();
+      }, 666)
+
+},1000);
+
+}
+}
 
 
+  
 
+var computer = new Computer();
+
+class Player {
+  constructor() {
+    this.score = 0;
+    this.playerTurn = false;
+    this.playerOrder = [];
+    this.good=true;
+    this.win=false;
+  }
+}
+
+var player = new Player();
 
 var turnCounter = document.querySelector("#score");
 var numberOne = document.querySelector("#one");
@@ -22,59 +141,181 @@ var numberSix = document.querySelector("#six");
 var numberSeven = document.querySelector("#seven");
 var numberEight = document.querySelector("#eight");
 var numberNine = document.querySelector("#nine");
+var numberTen = document.querySelector("#ten");
+var numberEleven = document.querySelector("#eleven");
+var numberTwelve = document.querySelector("#twelve");
+
+
+var gameOverScreen = document.querySelector(".game-over");
+var gameEndScreen =document.querySelector(".game-end");
+var gameBox = document.querySelector(".box");
+var restartbutton = document.querySelector("#restart");
 var startButton = document.querySelector("#start");
 
 
 
-
-
-startButton.addEventListener('click',(event)=>{
-     play();
-  
-});
-function play(){
-
-    win=false;
-    computerOrder =[];
-    playerOrder=[];
-    flash =0;
-    intervalId =0;
-    turn = 1;
-    turnCounter.value = turn-1;
-    good = true;
-     for(var i = 0;i<20;i++){
-        computerOrder.push(Math.floor(Math.random()*9+1));
-    }
-    compTurn = true;
-
-    intervalId = setInterval(gameTurn, 1800);
-
+function clearColorAfterPlayerClick(){
+  console.log(player.playerOrder, computer.flash)
+  if(!player.win){
+    setTimeout(()=>{
+     clearColor();
+     if (player.playerOrder.length === 0) {
+       gameTurn();
+     }
+    },900)
   }
-  function gameTurn() {
-    if (flash == turn) {
-      clearInterval(intervalId);
-      compTurn = false;
-      clearColor();
-     
-    }
-  if (compTurn) {
-    clearColor();
-    console.log("compturn ", computerOrder[flash]);
-    setTimeout(() => {
-      if (computerOrder[flash] == 1) one();
-      if (computerOrder[flash] == 2) two();
-      if (computerOrder[flash] == 3) three();
-      if (computerOrder[flash] == 4) four();
-      if (computerOrder[flash] == 5) five();
-      if (computerOrder[flash] == 6) six();
-      if (computerOrder[flash] == 7) seven();
-      if (computerOrder[flash] == 8) eight();
-      if (computerOrder[flash] == 9) nine();
-      flash++;
-    }, 400);
+
+}
+numberOne.addEventListener("click", event => {
+  if (computer.computerFlashing) {
+    return;
+  }
+
+  player.playerOrder.push(1);
+  check();
+  numberOne.style.backgroundColor = "lightblue";
+   clearColorAfterPlayerClick()
+}); 
+
+numberTwo.addEventListener("click", event => {
+  if (computer.computerFlashing) {
+    return;
+  }
+
+  player.playerOrder.push(2);
+  check();
+  numberTwo.style.backgroundColor = "lightblue";
+  clearColorAfterPlayerClick()
+});
+numberThree.addEventListener("click", event => {
+  if (computer.computerFlashing) {
+    return;
+  }
+
+  player.playerOrder.push(3);
+  check();
+  numberThree.style.backgroundColor = "lightblue";
+   clearColorAfterPlayerClick()
+});
+numberFour.addEventListener("click", event => {
+  if (computer.computerFlashing) {
+    return;
+  }
+
+  player.playerOrder.push(4);
+  check();
+  numberFour.style.backgroundColor = "lightblue";
+   clearColorAfterPlayerClick()
+});
+numberFive.addEventListener("click", event => {
+  if (computer.computerFlashing) {
+    return;
+  }
+
+  player.playerOrder.push(5);
+  check();
+  numberFive.style.backgroundColor = "lightblue";
+   clearColorAfterPlayerClick()
+});
+numberSix.addEventListener("click", event => {
+  if (computer.computerFlashing) {
+    return;
+  }
+
+  player.playerOrder.push(6);
+  check();
+  numberSix.style.backgroundColor = "lightblue";
+  clearColorAfterPlayerClick()
+});
+numberSeven.addEventListener("click", event => {
+  if (computer.computerFlashing) {
+    return;
+  }
+
+  player.playerOrder.push(7);
+  check();
+  numberSeven.style.backgroundColor = "lightblue";
+   clearColorAfterPlayerClick()
+});
+numberEight.addEventListener("click", event => {
+  if (computer.computerFlashing) {
+    return;
+  }
+
+  player.playerOrder.push(8);
+  check();
+  numberEight.style.backgroundColor = "lightblue";
+   clearColorAfterPlayerClick()
+});
+numberNine.addEventListener("click", event => {
+  if (computer.computerFlashing) {
+    return;
+  }
+
+  player.playerOrder.push(9);
+  check();
+  numberNine.style.backgroundColor = "lightblue";
+   clearColorAfterPlayerClick()
+});
+numberTen.addEventListener("click", event => {
+  if (computer.computerFlashing) {
+    return;
+  }
+
+  player.playerOrder.push("#");
+  check();
+  numberTen.style.backgroundColor = "lightblue";
+   clearColorAfterPlayerClick()
+});
+numberEleven.addEventListener("click", event => {
+  if (computer.computerFlashing) {
+    return;
+  }
+
+  player.playerOrder.push("@");
+  check();
+  numberEleven.style.backgroundColor = "lightblue";
+   clearColorAfterPlayerClick()
+});
+numberTwelve.addEventListener("click", event => {
+  if (computer.computerFlashing) {
+    return;
+  }
+
+  player.playerOrder.push("$");
+  check();
+  numberTwelve.style.backgroundColor = "lightblue";
+   clearColorAfterPlayerClick()
+});
+
+
+
+startButton.addEventListener("click", event => {
+  play();
+});
+
+
+function play() {
+  player.win = false;
+  turnCounter.value = computer.turn - 1;
+  player.good = true;
+  computer.createComputerOrder();
+  computer.computerTurn = true;
+  gameTurn();
+}
+
+
+function gameTurn() {
+  clearColor();
+  if(computer.flash==computer.turn){
+    computer.computerTurn = false;
+    
+  }
+  if (computer.computerTurn) {
+    computer.flashCards();
   }
 }
-  
+
 function clearColor() {
   numberOne.style.backgroundColor = "#100a1c";
   numberTwo.style.backgroundColor = "#100a1c";
@@ -85,224 +326,55 @@ function clearColor() {
   numberSeven.style.backgroundColor = "#100a1c";
   numberEight.style.backgroundColor = "#100a1c";
   numberNine.style.backgroundColor = "#100a1c";
-
+  numberTen.style.backgroundColor = "#100a1c";
+  numberEleven.style.backgroundColor = "#100a1c";
+  numberTwelve.style.backgroundColor = "#100a1c";
 }
-function one() {
-  
-  numberOne.style.backgroundColor = "lightblue";
-}
-function two() {
-  
-  numberTwo.style.backgroundColor = "lightblue";
-}
-
-function three() {
-  
-  numberThree.style.backgroundColor = "lightblue";
-}
-
-function four() {
-  
-  numberFour.style.backgroundColor = "lightblue";
-}
-
-function five() {
-  
-  numberFive.style.backgroundColor = "lightblue";
-}
-function six() {
-  
-  numberSix.style.backgroundColor = "lightblue";
-}
-function seven() {
-  
-  numberSeven.style.backgroundColor = "lightblue";
-}
-function eight() {
-  
-  numberEight.style.backgroundColor = "lightblue";
-}
-function nine() {
-  
-  numberNine.style.backgroundColor = "lightblue";
-}
-function flashColor() {
-  numberOne.style.backgroundColor = "lightblue";
-  numberTwo.style.backgroundColor = "lightblue";
-  numberThree.style.backgroundColor = "lightblue";
-  numberFour.style.backgroundColor = "lightblue";
-  numberFive.style.backgroundColor = "lightblue";
-  numberSix.style.backgroundColor = "lightblue";
-  numberSeven.style.backgroundColor = "lightblue";
-  numberEight.style.backgroundColor = "lightblue";
-  numberNine.style.backgroundColor = "lightblue";
-
-}  
-  
-numberOne.addEventListener('click', (event) => {
-  //if (on) {
-    playerOrder.push(1);
-    check();
-    one();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 900);
-    }
-  //}
-})
-
-numberTwo.addEventListener('click', (event) => {
-  //if (on) {
-    playerOrder.push(2);
-    check();
-    two();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 900);
-    }
-  //}
-})
-
-numberThree.addEventListener('click', (event) => {
-  //if (on) {
-    playerOrder.push(3);
-    check();
-    three();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 900);
-    }
- // }
-})
-
-numberFour.addEventListener('click', (event) => {
-  //if (on) {
-    playerOrder.push(4);
-    check();
-    four();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 900);
-    }
-  //}
-})
-numberFive.addEventListener('click', (event) => {
-  //if (on) {
-    playerOrder.push(5);
-    check();
-    five();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 900);
-    }
-  //}
-})
-
-numberSix.addEventListener('click', (event) => {
-  //if (on) {
-    playerOrder.push(6);
-    check();
-    six();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 900);
-    }
-  //}
-})
-
-numberSeven.addEventListener('click', (event) => {
-  //if (on) {
-    playerOrder.push(7);
-    check();
-    seven();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 900);
-    }
-  //}
-})
-
-numberEight.addEventListener('click', (event) => {
-  //if (on) {
-    playerOrder.push(8);
-    check();
-    eight();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 900);
-    }
- // }
-})
-
-numberNine.addEventListener('click', (event) => {
-  //if (on) {
-    playerOrder.push(9);
-    check();
-    nine();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 900);
-    }
- // }
-})
-
-
 function check() {
-  if (playerOrder[playerOrder.length - 1] !== computerOrder[playerOrder.length - 1])
-    {
-      good = false;
-      alert("You Loose");
-      console.log("when player order and comp order element value not match");
-      console.log("playerOrderValue  ",playerOrder[playerOrder.length - 1]);
-      console.log("computerOrderValue  ",computerOrder[playerOrder.length - 1]);
-      
-    }
-  if (playerOrder.length == 20 && good) {
-    console.log("WOn");
+  if (player.playerOrder[player.playerOrder.length - 1] != computer.computerOrder[player.playerOrder.length - 1]) {
+    player.good = false;
+    //alert("You loose  when player order and comporder value is not match");
+    gameOverScreen.style.display = "block";
+    gameBox.style.opacity = "0.4";
+    //gameBody.style.backgroundImage =.opacity = "0.5";
+
+
+    return;
+
+    //console.log(
+      //"player.playerOrder[player.playerOrder.length-1]    ",
+      //player.playerOrder[player.playerOrder.length - 1]
+    //);
+    //console.log(
+     // "computer.computerOrder[player.playerOrder.length-1]  ",
+     // computer.computerOrder[player.playerOrder.length - 1]
+   // );
+  }
+  if (player.playerOrder.length == 20 && player.good) {
+    player.win = true;
+    //alert("you won");
     winGame();
+    gameEndScreen.style.display = "block";
+    gameBox.style.opacity = "0.4";
   }
-
-  if (good == false) {
-    console.log("under if check good==false of check function");
-    flashColor();
-    //turnCounter.value = "NO!";
-    alert("Your score is "+ turnCounter.value);
-    // setTimeout(() => {
-    //   turnCounter.value = turn;
-    //   clearColor();
-    //   // play();
-    // }, 1000);
-
-
+   if (!player.good) {
+    
+    alert("under if check good==false of check function");
+   }
+  if (computer.turn == player.playerOrder.length && player.good && !player.win) {
+    console.log("'when player turn and not win");
+    computer.turn++;
+    player.playerOrder = [];
+    computer.computerTurn = true;
+    turnCounter.value = computer.turn - 1;
   }
-
-  if (turn == playerOrder.length && good && !win) {
-    console.log("when player turn and not win ");
-    turn++;
-    playerOrder = [];
-    compTurn = true;
-    flash = 0;
-    turnCounter.value = turn-1;
-    intervalId = setInterval(gameTurn, 800);
-  }
-
+}
+function winGame(){
+  player.win=true;
+  turnCounter.value = "Win!";
 }
 
-function winGame() {
-  flashColor();
-  turnCounter.value = "WIN!";
-  //on = false;
-  win = true;
-}
+
 
 
 
